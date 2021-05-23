207eb4bd34eea7968519c2dfe999cdafa1a7a073
jafet <jafet.manauth@hotmail.com>
2021-05-22 22:56:03 -0500

    created questions for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,58 @@
 const main = document.querySelector('#main');
-//click play
+var timer = document.createElement('h1')
+var secondsLeft = 60;
+var q1 = {
+    q:'Which of the following is not JavaScript Data Types?',
+    answers: 'Undefined Number Boolean Float',
+    correctAnswer: 'Float',
+};
+var q2 = {
+    q:'Inside which HTML element do we put the JavaScript?',
+    answers: '<script> <head> <meta> <style>',
+    correctAnswer: '<script>',
+};
+var q3 = {
+    q:'Which of the following is the correct syntax to display "Letsfindcourse" in an alert box using JavaScript?',
+    answers: 'alert-box("Letsfindcourse"); confirm("Letsfindcourse"); msgbox("Letsfindcourse"); alert("Letsfindcourse");',
+    correctAnswer: 'alert("Letsfindcourse");',
+};
+var q4 = {
+    q:'Javascript string using double quotes is exactly the same as a string using single quotes?',
+    answers: 'True False ',
+    correctAnswer: 'True',
+};
+var q5 = {
+    q:'Which of the following method checks if its argument is not a number?',
+    answers: 'isNaN() nonNaN() NaN()',
+    correctAnswer: 'isNaN()',
+};
+var q6 = {
+    q:'The _______ method of an Array object adds and/or removes elements from an array.',
+    answers: 'Reverse Shift Slice Splice',
+    correctAnswer: 'Splice',
+};
+var q7 = {
+    q:'In JavaScript, Arrays are data type. State True or False?',
+    answers: 'True False',
+    correctAnswer: 'False',
+};
+var q8 = {
+    q:'How to get a particular value using the tagged name?',
+    answers: 'getElementbyID() getElementsbyName() getElementsbyTagName() getTagName()',
+    correctAnswer: 'getElementsbyName()',
+};
+var q9 = {
+    q:'How to get a particular value using the tagged name?',
+    answers: 'getElementbyID() getElementsbyName() getElementsbyTagName() getTagName()',
+    correctAnswer: 'getElementbyID()',
+};
+var q10 = {
+    q:'Inside Which element do we link our CSS style sheets?',
+    answers: '<body> <p> <header> <head>',
+    correctAnswer: '<head>',
+};
+var listOfQuestions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
+
 //shows instructions
 //click start
 //timer starts running
@@ -29,8 +82,20 @@ function loadpage(){
     btn.addEventListener("click", startGame)
 };
 function startGame(){
+    //clears main, appends timer and starts timer
     clearMain(main);
-    console.log('sup foo');
+    var question = document.createElement('h1');
+    var answers = document.createElement('ul')
+    var individualAnswer = document.createElement('li')
+    
+    main.appendChild(timer);
+    setTime();
+    //pick random question
+    var randomQ = Math.floor(Math.random() * listOfQuestions.length);
+    console.log(randomQ);
+    listOfQuestions.splice(randomQ, 1)
+    console.log(listOfQuestions);
+
 };
 //clears main page
 function clearMain(parent) {
@@ -39,5 +104,16 @@ function clearMain(parent) {
     }
 };
 
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timer.textContent = secondsLeft + " seconds left.";
+  }, 1000);
+};
 
 loadpage();
+
+console.log(listOfQuestions[0])
+var res = q3["answers"].split(" ");
+console.log(res);
